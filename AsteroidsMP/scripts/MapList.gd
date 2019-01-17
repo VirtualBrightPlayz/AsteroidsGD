5bb40701e375062d8190d5898c6da80497baad38
Brian Zulch <lego.trains.b@gmail.com>
2019-01-16 20:57:33 -0700

    v0.1.4

--- AsteroidsMP/scripts/MapList.gd
+++ AsteroidsMP/scripts/MapList.gd
@@ -1,10 +1,16 @@
 extends ItemList
 
 func _ready():
+	set_process(true)
 	add_item("New Classic")
 	add_item("Slide Bye")
 	connect("item_selected", self, "_on_ItemList_item_selected")
 
+func _process(delta):
+	if Globals.touch:
+		rect_scale = Vector2(1, 2)
+	else:
+		rect_scale = Vector2(1, 1)
 
 func _on_ItemList_item_selected(index):
 	if index == 0:
